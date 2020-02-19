aa85c2b45452694c212aa1fea09de936d8032b55
teguh@gitlab.lab <teguh.h87@gmail.com>
2020-02-19 20:39:17 +0700

    doing some change

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
 # galaxy_merchant
 Hiring test for bitdna
+
+## introduction 
+This project porposed for simulating transfer transaction from individual to individual
+the transcaction follow sum similar convertion to the roman numerals
+
+the roman consist only seven numbers:
+````
+digits = {
+    'i': 1,
+    'v': 5,
+    'x': 10,
+    'l': 50,
+    'c': 100,
+    'd': 500,
+    'm': 1000
+}
+
+````
+
+## test application
+
+Scenario of based on what been determained by the tester 
+it have to consist some lines for picturing the transaction 
+accured.
+
+```
+lines = [
+        'glob is I prok is V pish is X tegj is L',
+        'glob glob Silver is 34 Credits',
+        'glob prok Gold is 57800 Credits',
+        'pish pish Iron is 3910 Credits',
+        'how much is pish tegj glob glob ?',
+        'how many Credits is glob prok Silver ?',
+        'how many Credits is glob prok Gold ?',
+        'how many Credits is glob prok Iron ?',
+        'how much wood could a woodchuck chuck if a woodchuck could chuck wood ?'
+    ]
+
+
+```
+
+# How to run script
+
+For runing this script you have to python inside your machine (PC)
+and if you don't have you must install it. Then you clone the repository and then 
+enter the directory
+
+Pleas run this comman for running this script
+
+```
+python main.py
+
+```
+
+## Result
+
+if you run command above it sould be has result like this:
+
+```
+pish tegj glob glob is 42
+globprok Silver is 68 Credits
+globprok Gold is 57800 Credits
+globprok Iron is 782 Credits
+I don't know idea what you talking about
+
+```
--- main.py
+++ main.py
@@ -1,97 +1,101 @@
 from roman_to_integer import digits
 
-num = {}
+class Main:
 
-goods = {}
+    def __init__(self):
+        self.num = {}
 
-operand = []
-operator = []
+        self.goods = {}
 
-def msg(message):
-    print(message)
+        self.operand = []
+        self.operator = []
 
-def parse(word):
+    def process(self, line):
+        word = line.lower().split(None)
 
-    num = {operand[i]: operator[i] for i, v in enumerate(operand)}
+        if not word or len(word) <= 0:
+            return
+            
+        if len(word) > 3 and word[0] in ['glob', 'prok', 'pish', 'tegj'] and not (word[-1] == 'credits'):
 
-    n = 0
-    try:
-        dig = [num[i] for i in word]
-    except:
-        return -1
-    while dig:
-        d = dig.pop(0)
-        if dig and dig[0] > d:
-            n -= d
+            for i in word:
+                if i in ['glob', 'prok', 'pish', 'tegj']:
+                    self.operand.append(i)
+                elif i in digits:
+                    self.operator.append(digits[i])
+
+            return
+        
+        if len(word) > 4 and word[-1] == 'credits' and word[-3] == 'is':
+            word.pop()
+            try:
+                val = float(word[-1])
+            except:
+                return self._msg("{0} is not valid numeric value".format(word[-1]))
+
+            word.pop()
+            word.pop()
+
+            g = word.pop()
+            n = self._parse(word)
+
+            self.goods[g] = val / n
+
+            return
+
+        if word[0:3] == ['how', 'much', 'is']:
+            word = word[3:]
+            if word[-1] == '?':
+                word.pop()
+
+            n = self._parse(word)
+
+            if n < 0:
+                return self._msg("{0} is not valid galatic number {1}".format(n, word))
+
+            print(' '.join(word), 'is', n)
+            
+            return
+        
+        if word[0:4] == ['how', 'many', 'credits', 'is']:
+            word = word[4:]
+            if word[-1] == '?':
+                word.pop()
+            g = word.pop()
+
+            if not g in self.goods:
+                return self._msg("I don't know the galaxy trading good {0}".format(g))
+            
+            n = self._parse(word)
+            if n < 0:
+                return self._msg("{0} is not valid galatic number {1}".format(n, word))
+            
+            print(''.join(word), g.title(), "is", int((n * self.goods[g])), "Credits")
         else:
-            n += d
-    return n
+            return self._msg("I don't know idea what you talking about")
 
-def process(line):
-    word = line.lower().split(None)
 
-    if not word or len(word) <= 0:
-        return
-        
-    if len(word) > 3 and word[0] in ['glob', 'prok', 'pish', 'tegj'] and not (word[-1] == 'credits'):
+    def _parse(self, word):
+        num = {self.operand[i]: self.operator[i] for i, v in enumerate(self.operand)}
 
-        for i in word:
-            if i in ['glob', 'prok', 'pish', 'tegj']:
-                operand.append(i)
-            elif i in digits:
-                operator.append(digits[i])
-
-        return
-    
-    if len(word) > 4 and word[-1] == 'credits' and word[-3] == 'is':
-        word.pop()
+        n = 0
         try:
-            val = float(word[-1])
+            dig = [num[i] for i in word]
         except:
-            return msg("{0} is not valid numeric value".format(word[-1]))
+            return -1
+        while dig:
+            d = dig.pop(0)
+            if dig and dig[0] > d:
+                n -= d
+            else:
+                n += d
+        return n
 
-        word.pop()
-        word.pop()
-
-        g = word.pop()
-        n = parse(word)
-
-        goods[g] = val / n
-
-        return
-
-    if word[0:3] == ['how', 'much', 'is']:
-        word = word[3:]
-        if word[-1] == '?':
-            word.pop()
-
-        n = parse(word)
-
-        if n < 0:
-            return msg("{0} is not valid galatic number {1}".format(n, word))
-
-        print(' '.join(word), 'is', n)
-        
-        return
-    
-    if word[0:4] == ['how', 'many', 'credits', 'is']:
-        word = word[4:]
-        if word[-1] == '?':
-            word.pop()
-        g = word.pop()
-
-        if not g in goods:
-            return msg("I don't know the galaxy trading good {0}".format(g))
-        
-        n = parse(word)
-        if n < 0:
-            return msg("{0} is not valid galatic number {1}".format(n, word))
-        
-        print(''.join(word), g.title(), "is", int((n * goods[g])), "Credits")
-    else:
-        return msg("I don't know idea what you talking about")
+    def _msg(self, s):
+        print(s)
 
 if __name__ == "__main__":
+    
     lines = [
         'glob is I prok is V pish is X tegj is L',
         'glob glob Silver is 34 Credits',
@@ -103,6 +107,7 @@ if __name__ == "__main__":
         'how many Credits is glob prok Iron ?',
         'how much wood could a woodchuck chuck if a woodchuck could chuck wood ?'
     ]
+    trx = Main()
 
     for line in lines:
-        process(line)
+        trx.process(line)
